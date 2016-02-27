ee0b7318baf19baf23e80a9ed766080833c93b79
flizzer <bdavis@macpro.local>
2016-02-27 17:28:36 -0500

    [#111816028] renaming files for more specificity

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,13 @@
 
 'use strict';
 var React = require('react-native');
-var RegistrationForm = require('./registrationForm.js');
+var VBSRegistrationForm = require('./VBSRegistrationForm.js');
 var { AppRegistry } = React;
 
 var QuestCaveVBS = React.createClass({
   render: function() {
       return (
-          <RegistrationForm/>
+          <VBSRegistrationForm/>
       );
   }
 });
